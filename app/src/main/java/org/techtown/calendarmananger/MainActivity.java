e30784e1228a508752371512917cf399a5da6f9c
Your Name <you@example.com>
2024-02-14 15:40:37 +0900

    memo1

--- app/src/main/java/org/techtown/calendarmananger/MainActivity.java
+++ app/src/main/java/org/techtown/calendarmananger/MainActivity.java
@@ -1,27 +1,66 @@
 package org.techtown.calendarmananger;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.graphics.Matrix;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
 
 public class MainActivity extends AppCompatActivity {
     // 위젯 전역변수
+    Button addButton;
     CalendarView calendar;
     TextView click_date;
+    TextView memo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // xml 연결
+        addButton = findViewById(R.id.addButton);
         calendar = findViewById(R.id.calendar);
         click_date = findViewById(R.id.click_date);
+        memo = findViewById(R.id.memo);
 
+        addButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder ad = new AlertDialog.Builder(MainActivity.this);
+                ad.setMessage("일정을 적어주세요!");
+                ad.setTitle("띠용");
+                final EditText et = new EditText(MainActivity.this);
+                ad.setView(et);
+
+                ad.setPositiveButton("저장", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        String result = et.getText().toString();
+                        memo.setText(result);
+                        dialogInterface.dismiss();
+                    }
+                });
+
+                ad.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        dialogInterface.dismiss();
+                    }
+                });
+
+                ad.show();
+            }
+        });
         // calendar 클릭 이벤트
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
